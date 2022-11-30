bd69b5d8f4103da7c7e062d78fd8864a916597b7
안경호 <akh1216@gmail.com>
2022-11-30 16:49:50 +0900

    FEAT: WebRTC

--- zoom/src/public/js/app.js
+++ zoom/src/public/js/app.js
@@ -1,6 +1,6 @@
 const socket = io();    //io function은 알아서 socket.io를 실행하고 있는 서버를 찾는다.
 
-const welcome = document.getElementById("welcome");
+const welcome = document.getElementById("welcome2");
 const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 
@@ -80,6 +80,101 @@ socket.on("room_change", (rooms) => {
     });
 });
 
+
+////////////////////////// video Face View
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute")
+const cameraBtn = document.getElementById("camera")
+const camerasSelect = document.getElementById("cameras");
+
+const welcome2 = document.getElementById("welcome");
+const call = document.getElementById("call");
+
+call.hidden = true;
+
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+
+async function getCameras() {
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        const currentCamera = myStream.getAudioTracks()[0]
+        cameras.forEach(camera => {
+            const option = document.createElement("option")
+            option.value = camera.deviceId;
+            if(currentCamera.label === camera.label) {
+                option.selected = true;
+            }
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        });
+    }catch(e){
+        console.log(e)
+    }
+}
+
+// videos를 다시 시작하는 방법
+async function getMedia(deviceId) {
+    const initialConstrains = {
+        audio: true, 
+        video: {facingMode: "user"},
+    };
+    const cameraConstraints = {
+        audio: true,
+        video: {deviceId: {exact: deviceId}},
+    };
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId? cameraConstraints : initialConstrains
+        );
+        myFace.srcObject = myStream
+        if(!deviceId) {
+            await getCameras();
+        }
+    } catch(e) {
+        console.log(e)
+    }
+}
+getMedia();
+
+function handleMuteClick() {
+    myStream.getAudioTracks().
+    forEach((track) => (track.enabled = !track.enabled));
+    if(!muted) {
+        muteBtn.innerText = "Unmute"
+        muted = true;
+    } else {
+        muteBtn.innerText = "Mute";
+        muted = false;
+    }
+}
+function handleCameraClick() {
+    myStream.
+    getVideoTracks().
+    forEach((track) => (track.enabled = !track.enabled));
+    if(cameraOff) {
+        cameraBtn.innerText = "Turn Camera Off";
+        cameraOff = false;
+    } else {
+        cameraBtn.innerText = "Turn Camera On";
+        cameraOff = true;
+    }
+}
+
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value);
+}
+
+muteBtn.addEventListener("click",handleMuteClick);
+cameraBtn.addEventListener("click",handleCameraClick);
+camerasSelect.addEventListener("input",handleCameraChange);
+
+
+/////////////// Websocket 
 // const messageList = document.querySelector("ul")
 // const nickForm = document.querySelector("#nick")
 // const messageForm = document.querySelector("#message")
